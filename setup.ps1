6a086da6b2239f96539fd784e86c3fe6f6281f9e
Mikel Muennekhoff <mmuennekhoff+github@gmail.com>
2021-05-27 13:25:54 +0200

    Add WSMan dependency for Exchange V2

--- setup.ps1
+++ setup.ps1
@@ -1,3 +1,7 @@
 Register-PackageSource -Trusted -ProviderName 'PowerShellGet' -Name 'Posh Test Gallery' -Location https://www.poshtestgallery.com/api/v2/
+Install-Module -Name PSWSMan -Force
 Install-Module AzureAD.Standard.Preview
 Install-Module -Name ExchangeOnlineManagement -Force
+
+Import-Module -Name PSWSMan
+Install-WSMan
